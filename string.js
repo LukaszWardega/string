25a3ff1817c700b91f567559d77b9f4b99f34259
Łukasz Wardęga <ukasz Wardga>
2016-10-02 15:40:59 +0200

    dodanie pliku

---
+++ string.js
@@ -0,0 +1,12 @@
+//table
+
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+
+animal = animal.toUpperCase();
+console.log(animal);
+
+text = text.replace('Papugi', animal);
+console.log(text);
+
+console.log(text.substr(0, text.length/2-1));
